61e26821c8bfab30c678ce2f830921907ad57c8e
Emilian Cebuc <cebucemiliano@gmail.com>
2018-02-17 02:32:53 +0000

    Current time display feature added to Phone App

--- wk1/PhoneApp/app/src/main/java/com/example/phoneapp/StartActivity.java
+++ wk1/PhoneApp/app/src/main/java/com/example/phoneapp/StartActivity.java
@@ -1,14 +1,24 @@
 package com.example.phoneapp;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button dialerButton;
+    TextView timeField;
+    Handler timeHandler;
+    Runnable timeRunnable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +26,20 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
 
         dialerButton = (Button)findViewById(R.id.dialerBtn);
         dialerButton.setOnClickListener(this);
+        timeField = (TextView)findViewById(R.id.clockView);
+
+        timeHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                timeField.setText(msg.getData().getString("Time"));
+            }
+        };
+        timeRunnable = new Runnable() {
+            @Override
+            public void run() {
+                updateTime();
+            }
+        };
+        timeHandler.postDelayed(timeRunnable, 1000);
     }
 
     public void onClick(View v){
@@ -29,5 +53,48 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     public void openDialer(){
         Intent openDialerIntent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(openDialerIntent);
+    }
+
+    public void updateTime() {
+        Message message = new Message();
+        Bundle bundle = new Bundle();
+
+        //get the current time of day, minutes and hours
+        Calendar now = Calendar.getInstance();
+        int hours = (int)now.get(Calendar.HOUR_OF_DAY);
+        int minutes = (int)now.get(Calendar.MINUTE);
+        int seconds = (int)now.get(Calendar.SECOND);
+
+        String sHours, sMinutes, secondsBlink, timeStamp;
+
+        //make sure to have leading zeroes for hour and minutes if < 9, and set them
+        if(hours <= 9){
+            sHours = "0" + Integer.toString(hours);
+        }
+        else{
+            sHours = Integer.toString(hours);
+        }
+
+        if(minutes <=9){
+            sMinutes = "0"+Integer.toString(minutes);
+        }
+        else{
+            sMinutes = Integer.toString(minutes);
+        }
+
+        //when the seconds are even, make the ':' separator appear, otherwise not
+        if(seconds%2 == 0){
+            secondsBlink = ":";
+        }
+        else{
+            secondsBlink = " ";
+        }
+
+        //send the time string to the handler, every 1 second
+        timeStamp = sHours + secondsBlink + sMinutes;
+        bundle.putString("Time", timeStamp);
+        message.setData(bundle);
+        timeHandler.sendMessage(message);
+        timeHandler.postDelayed(timeRunnable, 1000);
     }
 }
